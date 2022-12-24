bdf36c409510e1dc05c27107788f9861fecaf903
Max von der Gruen <m.vondergruen@protonmail.com>
2022-12-24 14:04:16 +0100

    bash: day 4 part 2

--- 2022/day4.sh
+++ 2022/day4.sh
@@ -6,9 +6,14 @@ while read line; do
 	END_FIRST=$(echo $line | cut -d ',' -f 1 | cut -d '-' -f 2)
 	START_SECOND=$(echo $line | cut -d ',' -f 2 | cut -d '-' -f 1)
 	END_SECOND=$(echo $line | cut -d ',' -f 2 | cut -d '-' -f 2)
-	if (( "$START_FIRST" >= "$START_SECOND" )) && (( "$END_FIRST" <= "$END_SECOND" )); then		
+
+	if (( "$START_FIRST" >= "$START_SECOND" )) && (( "$START_FIRST" <= "$END_SECOND" )); then
 		let INSPECT++
-	elif (( "$START_SECOND" >= "$START_FIRST" )) && (( "$END_SECOND" <= "$END_FIRST" )); then		
+	elif (( "$END_FIRST" >= "$START_SECOND" )) && (( "$END_FIRST" <= "$END_SECOND" )); then
+		let INSPECT++
+	elif (( "$START_SECOND" >= "$START_FIRST" )) && (( "$START_SECOND" <= "$END_FIRST" )); then
+		let INSPECT++
+	elif (( "$END_SECOND" >= "$START_FIRST" )) && (( "$END_SECOND" <= "$END_FIRST" )); then		
 		let INSPECT++
 	fi
 done < ./inputs/day4
